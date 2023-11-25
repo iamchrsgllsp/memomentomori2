04824ba22cb120a08825806ea1983c24dc365601
iamchrsgllsp <chrisgillespie2001@googlemail.com>
2023-11-25 12:54:43 +0000

    added login functionality

--- login.py
+++ login.py
@@ -2,6 +2,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import auth
+import requests
+import json
 
 
 # Fetch the service account key JSON file contents
@@ -13,8 +15,29 @@ firebase_admin.initialize_app(cred, {
 
 #auth.create_user(email="chrisgillespie2001@googlemail.com",password="testererowh")
 #etest = auth.get_user_by_email(email="chrisgillespie2002@googlemail.com")
-ref = db.reference('Database reference')
-print(ref.get())
+#ref = db.reference('Database reference')
+#print(ref.get())
 
+def check_user(email):
+    try:
+        user = auth.get_user_by_email(email)
+        return False
+    except:
+        return True
+    
+def add_user(email,password,display):
+    auth.create_user(email=email,password=password,display_name=display)
+    return True
+
+def sign_user_in(email, password):
+    url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDSAH8IvxM5e9S0tYAiTpMHW1As6UE8bxQ'
+    headers = {'Content-Type': 'application/json'}
+    data = {"email": email, "password": password}
+    resp = requests.post(url, json=data, headers=headers)
+    data = json.loads(resp.text)
+    if 'registered' in resp.text:
+        return {"data":data['displayName']}
+    else:
+        return False
 
 
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from flask import (Flask, render_template, send_from_directory, url_for,
-                   request, redirect, session)
+from flask import (
+    Flask,
+    render_template,
+    send_from_directory,
+    url_for,
+    request,
+    redirect,
+    session,
+)
 import os
 import requests
 import json
@@ -9,125 +16,157 @@ from invitecodes import codes
 from eatin import reslist
 import login
 
+
 app = Flask(__name__, static_folder="static")
+app.secret_key = "test"
 
-
-@app.route('/')
+@app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/find", methods=["GET", "POST"])
 def home():
-  if request.method == "POST":
-    postcode = request.form["postcode"]
-    return redirect(f"/found/{postcode}")
-  else:
-    return render_template("home.html")
+    if request.method == "POST":
+        postcode = request.form["postcode"]
+        return redirect(f"/found/{postcode}")
+    else:
+        if session:
+            return render_template("home.html",user=session['user'])
+        else:
+            return redirect("/")
 
 
 @app.route("/found/<postcode>")
 def found(postcode):
-  justeat = "https://uk.api.just-eat.io/restaurants/bypostcode/" + postcode
-  data = requests.get(
-      justeat,
-      headers={
-          "Content-Type":
-          "text/html",
-          "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-      },
-  )
-  data = json.loads(data.content)
-  data = data["Restaurants"]
-  return render_template("restaurants2.html", restaurants=data)
+    justeat = "https://uk.api.just-eat.io/restaurants/bypostcode/" + postcode
+    data = requests.get(
+        justeat,
+        headers={
+            "Content-Type": "text/html",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+        },
+    )
+    data = json.loads(data.content)
+    data = data["Restaurants"]
+    return render_template("restaurants2.html", restaurants=data)
 
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+    return render_template("about.html")
+
 
 @app.route("/partners")
 def partners():
-  return render_template("partner.html")
+    return render_template("partner.html")
+
 
 @app.route("/rest/<name>")
 def resthub(name):
-  data = [x for x in reslist if str(x["id"]) == name]
-  print(data)
-  if data == []:
-    return "invalid"
-  else:
-    return render_template("grumblepartner.html", data=data[0])
+    data = [x for x in reslist if str(x["id"]) == name]
+    print(data)
+    if data == []:
+        return "invalid"
+    else:
+        return render_template("grumblepartner.html", data=data[0])
 
-@app.route('/match', methods=["GET", "POST"])
+
+@app.route("/match", methods=["GET", "POST"])
 def matcher():
-  data = match.matcher(1)
-  return render_template('found.html', data=data)
+    data = match.matcher(1)
+    return render_template("found.html", data=data)
 
 
-@app.route('/invite', methods=['GET', 'POST'])
+@app.route("/invite", methods=["GET", "POST"])
 def inviter():
-  if request.method == "POST":
-    email = request.form['email']
-    print(email)
-    geninvite = uuid.uuid4()
-    with open("test.txt", "w") as f:
-      f.write(str(geninvite))
-      codes.append({"userid": 1, "email": email, "invitecode": str(geninvite)})
-    return f"Email has been sent to {email}<br>User can click: <a href='https://grumble.chrisgillespie1.repl.co/invited/{geninvite}'>here</a> to join the meetin"
-  else:
-    return render_template("invite.html")
+    if request.method == "POST":
+        email = request.form["email"]
+        print(email)
+        geninvite = uuid.uuid4()
+        with open("test.txt", "w") as f:
+            f.write(str(geninvite))
+            codes.append({"userid": 1, "email": email, "invitecode": str(geninvite)})
+        return f"Email has been sent to {email}<br>User can click: <a href='https://grumble.chrisgillespie1.repl.co/invited/{geninvite}'>here</a> to join the meetin"
+    else:
+        return render_template("invite.html")
 
 
-@app.route('/invited/<invitecode>', methods=["GET", "POST"])
+@app.route("/invited/<invitecode>", methods=["GET", "POST"])
 def invited(invitecode):
-  data = ""
-  if request.method == "POST":
-    email = request.form['email']
-    for i in codes:
-      if i['email'] == email:
-        data = "success"
-    return data
-  else:
-    return render_template("invitecode.html")
+    data = ""
+    if request.method == "POST":
+        email = request.form["email"]
+        for i in codes:
+            if i["email"] == email:
+                data = "success"
+        return data
+    else:
+        return render_template("invitecode.html")
 
 
-@app.route('/eatin')
+@app.route("/eatin")
 def eatin():
-  featured = [x for x in reslist if x["isfeatured"] == True]
-  return render_template("eatin.html", added=reslist, featured=featured)
+    featured = [x for x in reslist if x["isfeatured"] == True]
+    return render_template("eatin.html", added=reslist, featured=featured)
 
 
 @app.route("/favicon.ico")
 def favicon():
-  return send_from_directory(
-      os.path.join(app.root_path, "static"),
-      "images/favi.ico",
-      mimetype="image/vnd.microsoft.icon",
-  )
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "images/favi.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
-@app.route('/login',methods=["GET","POST"])
-def login():
-  if request.method == "POST":
-    return "post test"
-  else:
-    return render_template('login.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def loginform():
+    if request.method == "POST":
+        data = login.sign_user_in(request.form['email'],request.form['password'])
+        if data != False:
+          session['user'] = data['data']
+          return redirect(url_for("home"))
+        else:
+            return redirect(url_for("loginform"))
+    else:
+        return render_template("login.html")
+  
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signupform():
+    if request.method == "POST":
+        user = login.check_user(request.form["email"])
+        if user:
+            new = login.add_user(request.form["email"], request.form["password"],request.form['display'])
+            if new:
+                session['user'] = request.form['display']
+                return redirect(url_for("home"))
+            else:
+                return redirect(url_for("signupform"))
+        else:
+            return redirect(url_for("signupform"))
+    else:
+        return render_template("signup.html")
+
 
 @app.route("/justeat")
 def jetester():
-  justeat = "https://uk.api.just-eat.io/restaurants/bypostcode/bt126hz"
-  data = requests.get(
-      justeat,
-      headers={
-          "Content-Type":
-          "text/html",
-          "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-      },
-  )
-  data = json.loads(data.content)
-  data = data["Restaurants"]
-  return data
+    justeat = "https://uk.api.just-eat.io/restaurants/bypostcode/bt126hz"
+    data = requests.get(
+        justeat,
+        headers={
+            "Content-Type": "text/html",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+        },
+    )
+    data = json.loads(data.content)
+    data = data["Restaurants"]
+    return data
 
-app.run(host='localhost', port=81, debug=True)
+
+app.run(host="localhost", port=81, debug=True)
